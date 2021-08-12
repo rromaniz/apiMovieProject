95442991e3a518c8abd83e491b4e668f718a4612
rromaniz <romaniz.avila@gmail.com>
2021-08-11 20:02:00 -0500

    Agregue funcion que crea las cards dinamicamente y sus estilos CSS tambien

--- js/main.js
+++ js/main.js
@@ -2,16 +2,61 @@
 const url_base ='https://api.themoviedb.org/3/';
 const language ='es-ES';
 const api_key ='a7223a9370b830ebb4d41859e186a0e0';
-const image_url_base ='https://image.tmdb.org/t/p/w500/'; ///url base para visualizar las imagenes que devuelve el api
+const image_url_base ='https://image.tmdb.org/t/p/w500'; ///url base para visualizar las imagenes que devuelve el api
+const container_cards = document.getElementById('principal');
 
+
+
+// función que crea las cards para mostrar las películas
+const createCards =(pelicula,deck_num)=>{
+    let deck = document.getElementById(`deck-${deck_num}`);
+    //card
+    let movieCard = document.createElement('div');
+    movieCard.className="card";
+    movieCard.style.width="14rem";
+    //imagen
+    let imageCard = document.createElement('img');
+    imageCard.className ="card-img-top";
+    imageCard.src =`${image_url_base}${pelicula.poster_path}`;
+    //body
+    let bodyCard = document.createElement('div');
+    bodyCard.className ="body-card text-center";
+    //Títulos y texto
+    let titleCard = document.createElement('p');
+    titleCard.className="title";
+    titleCard.innerHTML =`${pelicula.title}`;
+    let bodyText = document.createElement('p');
+    bodyText.className="vote";
+    bodyText.innerHTML=`&#11088; ${pelicula.vote_average}| ${pelicula.release_date}`;
+
+    bodyCard.appendChild(titleCard);
+    bodyCard.appendChild(bodyText);
+    movieCard.appendChild(imageCard);
+    movieCard.appendChild(bodyCard);
+    deck.appendChild(movieCard);
+
+}
 
 ////Función que hace fetch de las peliculas populares
 const peliculas_Populares =()=>{
     axios({
         method:'GET',
-        url:`${url_base}movie/popular?api_key=${api_key}&language=es-ES`
+        url:`${url_base}movie/now_playing?api_key=${api_key}&language=es-ES`
     }).then(response=>{
         console.log(response.data.results);
+        let count=0;
+        let deck_number =0;
+        response.data.results.forEach((pelicula,index) => {
+            createCards(pelicula,deck_number);
+            count++;
+            if(count%5 == 0){
+            deck_number++;
+            new_deck = document.createElement('div');
+            new_deck.className='card-deck';
+            new_deck.id=`deck-${deck_number}`;
+            container_cards.appendChild(new_deck);
+            }                  
+        });
     }).catch(error=>console.log(error))
 }
 
@@ -42,4 +87,3 @@ const detalles_pelicula=(id)=>{
 peliculas_Populares();
 busqueda_peliculas('Suicide Squad');
 detalles_pelicula('436969');
- 
