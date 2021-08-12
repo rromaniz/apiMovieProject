bda8c6f0b78c5f6d65029874968130a6d2d8844d
rromaniz <romaniz.avila@gmail.com>
2021-08-11 20:14:51 -0500

    Agregue nuevo JS para la pagina de cartelera

--- js/cartelera.js
+++ js/cartelera.js
@@ -1,4 +1,3 @@
-
 const url_base ='https://api.themoviedb.org/3/';
 const language ='es-ES';
 const api_key ='a7223a9370b830ebb4d41859e186a0e0';
@@ -37,11 +36,11 @@ const createCards =(pelicula,deck_num)=>{
 
 }
 
-////Función que hace fetch de las peliculas populares
-const peliculas_Populares =()=>{
+
+const peliculas_Cartelera =()=>{
     axios({
         method:'GET',
-        url:`${url_base}movie/popular?api_key=${api_key}&language=es-ES`
+        url:`${url_base}movie/now_playing?api_key=${api_key}&language=es-ES`
     }).then(response=>{
         console.log(response.data.results);
         let count=0;
@@ -60,30 +59,5 @@ const peliculas_Populares =()=>{
     }).catch(error=>console.log(error))
 }
 
-///Función que realiza el proceso de busqueda 
-///Recibe el parametro query que es la palabra que va a buscar y devuelve las peliculas que contengan esa palabra
-const busqueda_peliculas =(query)=>{
-    axios({
-        method:'GET',
-        url:`${url_base}search/movie?api_key=${api_key}&language=es-ES&query=${query}`
-    }).then(response=>{
-        console.log(response.data.results);
-    }).catch(error=>console.log(error));
 
-}
-
-///Función que recibe detalles de una película en especifico recibiendo el ID
-const detalles_pelicula=(id)=>{
-    axios({
-     method:'GET',
-     url:`${url_base}movie/${id}?api_key=${api_key}&language=es-ES`
-    }).then(response=>{
-        console.log(response.data);
-    }).catch(error=>console.log(error));
-}
-
-
-
-peliculas_Populares();
-busqueda_peliculas('Suicide Squad');
-detalles_pelicula('436969');
+peliculas_Cartelera();
